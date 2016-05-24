c89dfcb3a19705efdca52c667b573098ab999e2c
Phil Banks <phil@helpfultechnology.com>
2016-05-24 12:00:57 +0100

    Initial commit
    Update credits with fork info.
    Make footer nav secondary.

--- footer.php
+++ footer.php
@@ -14,15 +14,15 @@
         <a class="back-to-top" href="#">&uarr;</a>
         <div class="container">
             <div>
-                <nav id="site-navigation" class="main-navigation" role="navigation">
-			        <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+                <nav class="secondary-navigation" role="navigation">
+			        <?php wp_nav_menu( array( 'theme_location' => 'secondary' ) ); ?>
 		        </nav><!-- #site-navigation -->
             </div>
 		    <div class="site-info">
 			    <?php do_action( 'fastr_credits' ); ?>
 			    <?php printf( __( 'Proudly powered by %s', 'fastr' ), '<a href="http://wordpress.org/" rel="generator"  target="_blank">WordPress</a>' ); ?>
 			    <span class="sep"> | </span>
-			    <?php printf( __( 'Theme: %1$s by %2$s.', 'fastr' ), 'Fastr', '<a href="http://kanishkkunal.in" rel="designer" target="_blank">Kanishk</a>' ); ?>
+			    <?php printf( __( 'Theme: %1$s by %2$s.', 'fastr' ), 'fork of Fastr', '<a href="http://kanishkkunal.in" rel="designer" target="_blank">Kanishk</a>' ); ?>
 		    </div><!-- .site-info -->
         </div>
 	</footer><!-- #colophon -->
